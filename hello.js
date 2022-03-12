758a25a9580a08f920f9043c60a9973355502c8e
Emma Grannis <grannis.emma@gmail.com>
2022-03-12 23:15:45 +0000

    Saving initial changes so far

---
+++ hello.js
@@ -0,0 +1,13 @@
+/* const sayHello  = function () {
+  console.log("Hello, world");
+}
+
+sayHello(); */
+// commented out above for second function expression to work properly
+
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+};
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
